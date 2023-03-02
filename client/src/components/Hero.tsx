34c7cc670c7e4c5bce811daccdc6551db3e7b0f4
Assami Muzaki <muzakiassami@gmail.com>
2023-03-02 20:32:07 +0700

    change index page

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,37 +1,40 @@
+import Image from "next/image";
+
 export default function Hero() {
   return (
     <div className="bg-slate-100">
-      <div className="container">
-        <div className="w-full flex flex-col justify-center items-center gap-4 py-32 lg:px-32">
-          <h1 className="font-semibold text-2xl text-center lg:text-2xl text-slate-900 mb-4">
-            a Place for my thoughts, finds, and opinions
-          </h1>
-          <p className="text-slate-400 text-sm">tags: </p>
-          <div className="flex gap-4 justify-center">
-            <a
-              href=""
-              className="px-4 py-1 bg-transparent border rounded border-slate-500 text-slate-500 hover:bg-slate-500 hover:text-white transition duration-300 ease-in-out"
-            >
-              all
-            </a>
-            <a
-              href=""
-              className="px-4 py-1 bg-transparent border rounded border-slate-500 text-slate-500 hover:bg-slate-500 hover:text-white transition duration-300 ease-in-out"
-            >
-              backend
-            </a>
-            <a
-              href=""
-              className="px-4 py-1 bg-transparent border rounded border-slate-500 text-slate-500 hover:bg-slate-500 hover:text-white transition duration-300 ease-in-out"
-            >
-              music
-            </a>
-            <a
-              href=""
-              className="px-4 py-1 bg-transparent border rounded border-slate-500 text-slate-500 hover:bg-slate-500 hover:text-white transition duration-300 ease-in-out"
-            >
-              tips
-            </a>
+      <div className="container sticky top-0">
+        <div className="w-full">
+          <div className="hero-profile pt-12 pb-4 px-8">
+            <div className="w-full flex justify-center">
+              <Image
+                src="/imgs/upme.jpg"
+                alt="Profile picture"
+                width={200}
+                height={200}
+                className="rounded-md shadow-lg mb-12 text-center"
+              />
+            </div>
+
+            <h2 className="text-xl font-semibold text-slate-800">
+              Assami Muzaki
+            </h2>
+            <p className="font-light text-slate-600">
+              Passionate Backend Developer <br />{" "}
+              <span className="font-normal text-sm">(Node JS)</span>
+            </p>
+          </div>
+          <div className="hero-about px-8">
+            <h2 className="text-xl font-semibold text-slate-700">About</h2>
+            <p className="text-sm text-slate-500 tracking-wide">
+              Distiction Graduate Student at Bangkit Academy. Proificient at
+              Server-Side Programming such as Creating RESTfulAPI, Databases
+              Management, Unit Test, etc. <br />
+            </p>
+            <p className="text-sm text-slate-500 tracking-wide pt-2">
+              Currently focusing on{" "}
+              <span className="font-bold">Express JS</span>
+            </p>
           </div>
         </div>
       </div>
