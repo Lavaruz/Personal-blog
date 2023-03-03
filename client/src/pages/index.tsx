f12a6adbcdd0b4eefc694a0f6dcbae17fb24520e
Assami Muzaki <muzakiassami@gmail.com>
2023-03-03 21:20:16 +0700

    complete detailed blog

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -18,28 +18,4 @@ interface BlogProps {
   blogPost: Post[];
 }
 
-export default function Home(props: BlogProps) {
-  const { blogPost } = props;
-  return (
-    <>
-      <div className="">
-        {/* <Navbar></Navbar> */}
-        <div className="lg:grid lg:grid-cols-[1fr_max-content_.4fr] flex flex-col-reverse">
-          <Blogs blogs={blogPost}></Blogs>
-          <div className="hidden lg:block lg:h-screen lg:w-0.5 lg:mx-2 lg:bg-slate-100 lg:sticky lg:top-0"></div>
-          <Hero></Hero>
-        </div>
-        {/* <Footer></Footer> */}
-      </div>
-    </>
-  );
-}
-
-export async function getServerSideProps() {
-  // Fetch data from external API
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-  const blogPost = await res.json();
-
-  // Pass data to the page via props
-  return { props: { blogPost } };
-}
+export default function Home(props: BlogProps) {}
